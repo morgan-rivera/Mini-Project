2ae13669316458d5b2ca553f44278624d9621473
Morgan Rivera <morganator03@gmail.com>
2021-06-28 14:29:19 -0400

    Initial Commit

---
+++ Mini Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mini Project
+//
+//  Created by Morgan Rivera on 6/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
